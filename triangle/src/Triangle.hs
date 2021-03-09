170bfe5cdd5232adb3d35e772625baed10eee069
miguelbpgh <mbpereira94@gmail.com>
2021-03-09 22:49:18 +0000

    triangles

--- triangle/src/Triangle.hs
+++ triangle/src/Triangle.hs
@@ -7,4 +7,15 @@ data TriangleType = Equilateral
                   deriving (Eq, Show)
 
 triangleType :: (Num a, Ord a) => a -> a -> a -> TriangleType
-triangleType a b c = error "You need to implement this function."
+triangleType a b c
+    | isIllegal     = Illegal
+    | isEquilateral = Equilateral
+    | isIsosceles   = Isosceles
+    | isScalene     = Scalene
+    | otherwise     = Illegal
+    where
+        isIllegal     = not (a > 0 && b > 0 && c > 0 && a + b > c && b + c > a && c + a > b )
+        nEqualities   = length (filter (==True) [a == b, a == c, b == c])
+        isEquilateral = nEqualities == 3
+        isIsosceles   = nEqualities == 1
+        isScalene     = nEqualities == 0
