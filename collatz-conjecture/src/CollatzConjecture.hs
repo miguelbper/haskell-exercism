18ebb0a950933f5c6641faca92204d8998c2f1dd
miguelbpgh <mbpereira94@gmail.com>
2021-03-09 17:27:59 +0000

    collatz-conjecture

--- collatz-conjecture/src/CollatzConjecture.hs
+++ collatz-conjecture/src/CollatzConjecture.hs
@@ -1,4 +1,9 @@
 module CollatzConjecture (collatz) where
 
 collatz :: Integer -> Maybe Integer
-collatz = error "You need to implement this function."
+collatz n
+    | n <= 0 = Nothing 
+    | n == 1 = Just 0
+    | even n = (+1) <$> collatz (n `div` 2)
+    | odd  n = (+1) <$> collatz (3 * n + 1)
+    | otherwise = Nothing
