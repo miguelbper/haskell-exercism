727cf20a373fa50e0402377a84c8cfd7de5a4368
miguelbpgh <mbpereira94@gmail.com>
2021-03-09 21:58:45 +0000

    sum of multiples

--- sum-of-multiples/src/SumOfMultiples.hs
+++ sum-of-multiples/src/SumOfMultiples.hs
@@ -1,4 +1,4 @@
 module SumOfMultiples (sumOfMultiples) where
 
 sumOfMultiples :: [Integer] -> Integer -> Integer
-sumOfMultiples factors limit = error "You need to implement this function."
+sumOfMultiples factors limit = sum [ x | x <- [1..limit - 1], any (\y -> x `mod` y == 0) (filter (> 0) factors) ]
