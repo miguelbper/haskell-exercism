a7d757de184512f1b56e51e17d543569bff57025
miguelbpgh <mbpereira94@gmail.com>
2021-03-11 19:43:08 +0000

    pythagorean triplet

--- pythagorean-triplet/src/Triplet.hs
+++ pythagorean-triplet/src/Triplet.hs
@@ -1,4 +1,8 @@
 module Triplet (tripletsWithSum) where
 
 tripletsWithSum :: Int -> [(Int, Int, Int)]
-tripletsWithSum sum = error "You need to implement this function."
+tripletsWithSum n = [ (a,b,c) | c <- [1..(floor ((1 / sqrt 2) * fromIntegral n))], 
+                                b <- [(ceiling (fromIntegral (n - c) / 2))..(n - c - 1)], 
+                                let a = n - b - c, 
+                                a^2 + b^2 == c^2 ]
+                            
