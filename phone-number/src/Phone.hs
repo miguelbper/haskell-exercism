a2868e8f3da6a40b1696afb6b12f6049073f3faa
miguelbpgh <mbpereira94@gmail.com>
2021-03-17 11:22:14 +0000

    phone

--- phone-number/src/Phone.hs
+++ phone-number/src/Phone.hs
@@ -1,4 +1,18 @@
 module Phone (number) where
 
 number :: String -> Maybe String
-number xs = error "You need to implement this function."
+number xs
+    | valid     = Just str'
+    | otherwise = Nothing
+    where
+        xstr = ['0'..'9']
+        nstr = ['2'..'9']
+        
+        str  = filter (`elem` xstr) xs
+        str' = reverse . take 10 . reverse $ str
+        
+        len = length str
+        
+        valid =    (head str' `elem` nstr)
+                && (str' !! 3 `elem` nstr)
+                && (len == 10 || (len == 11 && head str == '1'))
