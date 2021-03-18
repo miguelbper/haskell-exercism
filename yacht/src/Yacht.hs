29c73d98ad243b87cde45f6b14d9f3253bcf0f0f
miguelbpgh <mbpereira94@gmail.com>
2021-03-18 11:41:07 +0000

    yacht 2

--- yacht/src/Yacht.hs
+++ yacht/src/Yacht.hs
@@ -1,6 +1,6 @@
 module Yacht (yacht, Category(..)) where
 
-import Data.List
+import Data.List ( sort, group )
 
 data Category = Ones
               | Twos
@@ -24,21 +24,10 @@ yacht cat dice
     | cat == Fours          = (* 4) . length . filter (==4) $ dice
     | cat == Fives          = (* 5) . length . filter (==5) $ dice
     | cat == Sixes          = (* 6) . length . filter (==6) $ dice
-    | cat == LittleStraight = if sort dice == [1,2,3,4,5] then 30       else 0
-    | cat == BigStraight    = if sort dice == [2,3,4,5,6] then 30       else 0
-    | cat == Yacht          = if allEq dice               then 50       else 0
-    | cat == FullHouse      = if fullHouse                then sum dice else 0
-    | cat == FourOfAKind    = 
-        case () of 
-            () | allEq . take 4 . sort $ dice -> sum . take 4 . sort $ dice
-            () | allEq . drop 1 . sort $ dice -> sum . drop 1 . sort $ dice
-            () | True                         -> 0
+    | cat == LittleStraight = if sort dice == [1,2,3,4,5] then 30 else 0
+    | cat == BigStraight    = if sort dice == [2,3,4,5,6] then 30 else 0
+    | cat == Yacht          = if all (== head dice) dice  then 50 else 0
+    | cat == FullHouse      = if (== [2,3]) . sort . map length . group . sort $ dice then sum dice else 0
+    | cat == FourOfAKind    = (\x -> if null x then 0 else head x) . map (sum . take 4) . filter ((>= 4) . length) . group . sort $ dice
     | cat == Choice         = sum dice
     | otherwise             = 0
-    where
-        fullHouse = (not . allEq $ dice)
-                    && (   ((allEq . take 3 . sort $ dice) && (allEq . drop 3 . sort $ dice))
-                        || ((allEq . take 2 . sort $ dice) && (allEq . drop 2 . sort $ dice)))
-
-        allEq :: (Eq a) => [a] -> Bool
-        allEq lst = all (== head lst) lst
