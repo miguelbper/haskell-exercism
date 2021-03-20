0809de34d8e1846f3eb7043886f97f80f47d8eda
miguelbpgh <mbpereira94@gmail.com>
2021-03-20 01:39:14 +0000

    grade school

--- grade-school/src/School.hs
+++ grade-school/src/School.hs
@@ -1,15 +1,38 @@
 module School (School, add, empty, grade, sorted) where
 
-data School = Dummy
+import Data.List ( sort, sortBy, groupBy )
 
-add :: Int -> String -> School -> School
-add gradeNum student school = error "You need to implement this function."
+type Grade    = Int
+type Name     = String
+type Student  = (Grade, Name)
+type Students = [Student]
+
+newtype School = School Students
+
+add :: Grade -> Name -> School -> School
+add grade name (School students) = School ((grade, name) : students)
 
 empty :: School
-empty = error "You need to implement this function."
+empty = School []
 
-grade :: Int -> School -> [String]
-grade gradeNum school = error "You need to implement this function."
+grade :: Grade -> School -> [Name]
+grade grade (School students) = sort [ snd student | student <- students, fst student == grade ]
 
-sorted :: School -> [(Int, [String])]
-sorted school = error "You need to implement this function."
+sorted :: School -> [(Grade, [Name])]
+sorted (School students) = map conv . group' . sortBy ordGrade . sortBy ordName $ students
+    where
+        mapTuple :: (a -> b) -> (a, a) -> (b, b)
+        mapTuple f (a1, a2) = (f a1, f a2)
+
+        ordName :: Student -> Student -> Ordering
+        ordName = curry $ uncurry compare . mapTuple snd
+
+        ordGrade :: Student -> Student -> Ordering
+        ordGrade = curry $ uncurry compare . mapTuple fst
+
+        group' :: Students -> [Students]
+        group' = groupBy (\x y -> fst x == fst y)
+
+        conv :: Students -> (Grade, [Name])
+        conv xs = (fst . head $ xs, map snd xs)
+
