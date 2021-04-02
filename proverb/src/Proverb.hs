9648c1bb3674ab8f004dac17000c89d4805d7099
miguelbpgh <mbpereira94@gmail.com>
2021-04-02 14:39:21 +0100

    proverb

--- proverb/src/Proverb.hs
+++ proverb/src/Proverb.hs
@@ -1,4 +1,17 @@
 module Proverb(recite) where
 
 recite :: [String] -> String
-recite = error "You need to implement this function!"
+recite [] = ""
+recite (x:ys) = firstLines (x:ys) ++ lastLine x
+    where
+        lastLine x = "And all for the want of a " ++ x ++ "."
+
+        firstLines = concatMap line . break 2
+
+        break :: Int -> [a] -> [[a]]
+        break n ys
+            | n > length ys = []
+            | otherwise     = take n ys : break n (drop 1 ys)
+
+        line :: [String] -> String
+        line ys = "For want of a " ++ head ys ++ " the " ++ ys !! 1 ++ " was lost.\n"
