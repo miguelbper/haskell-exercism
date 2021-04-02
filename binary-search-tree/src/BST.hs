80b62d9abccc2bf6bbbbb9a82f52c706be168d94
miguelbpgh <mbpereira94@gmail.com>
2021-04-02 19:26:25 +0100

    binary tree with folds

--- binary-search-tree/src/BST.hs
+++ binary-search-tree/src/BST.hs
@@ -15,27 +15,27 @@ data BST a = Nil
                    left  :: BST a,
                    right :: BST a} deriving (Eq, Show)
 
+instance Foldable BST where
+    foldr _ acc Nil         = acc
+    foldr f acc (BST a l r) = foldr f (f a (foldr f acc r)) l
+
 bstLeft :: BST a -> Maybe (BST a)
-bstLeft Nil = Nothing
+bstLeft Nil  = Nothing
 bstLeft tree = Just . left $ tree
 
 bstRight :: BST a -> Maybe (BST a)
-bstRight Nil = Nothing
+bstRight Nil  = Nothing
 bstRight tree = Just . right $ tree
 
 bstValue :: BST a -> Maybe a
-bstValue Nil = Nothing
+bstValue Nil  = Nothing
 bstValue tree = Just . value $ tree
 
 empty :: BST a
 empty = Nil
 
-fromList :: Ord a => [a] -> BST a
-fromList [] = Nil
-fromList (x:xs) = BST x (fromList xsSmaller) (fromList xsGreater)
-    where
-        xsSmaller = filter (x >=) xs
-        xsGreater = filter (x < ) xs
+singleton :: a -> BST a
+singleton x = BST x Nil Nil
 
 insert :: Ord a => a -> BST a -> BST a
 insert x Nil = singleton x
@@ -44,9 +44,8 @@ insert x (BST y l r)
     | x >  y    = BST y l (insert x r)
     | otherwise = BST y l r
 
-singleton :: a -> BST a
-singleton x = BST x Nil Nil
-
 toList :: BST a -> [a]
-toList Nil = []
-toList (BST x l r) = toList l ++ [x] ++ toList r 
+toList = foldr (:) []
+
+fromList :: Ord a => [a] -> BST a
+fromList = foldl (flip insert) empty
