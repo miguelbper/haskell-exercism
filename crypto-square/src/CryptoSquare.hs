c962630a08d3ee25ce95f8521d8032e629e8f907
miguelbpgh <mbpereira94@gmail.com>
2021-04-02 16:43:58 +0100

    crypto square

--- crypto-square/src/CryptoSquare.hs
+++ crypto-square/src/CryptoSquare.hs
@@ -1,4 +1,26 @@
 module CryptoSquare (encode) where
 
+import Data.List ( transpose )
+import Data.List.Split ( chunksOf )
+import Data.Char ( toLower )
+import Control.Arrow ( (&&&) )
+
 encode :: String -> String
-encode xs = error "You need to implement this function."
+encode = unwords
+       . transpose
+       . uncurry chunksOf
+       . (&&&) chunkSize id
+       . map toLower
+       . removePunctuation
+    where
+        punctuation = ",;.:-_~^´`!?@%#$& "
+
+        removePunctuation :: String -> String
+        removePunctuation = filter (not . flip elem punctuation)
+
+        chunkSize :: String -> Int
+        chunkSize xs = c
+            where
+                l = length . removePunctuation $ xs
+                s = floor . sqrt . fromIntegral $ l
+                c = s + (if l > s^2 then 1 else 0)
