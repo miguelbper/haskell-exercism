151307e67e72da3ac48dd3c069de4a91dc761c19
miguelbpgh <mbpereira94@gmail.com>
2021-04-02 15:41:38 +0100

    binary search

--- binary-search/src/BinarySearch.hs
+++ binary-search/src/BinarySearch.hs
@@ -1,6 +1,19 @@
 module BinarySearch (find) where
 
-import Data.Array
+import Data.Array ( Array, bounds, (!) )
 
 find :: Ord a => Array Int a -> a -> Maybe Int
-find array x = error "You need to implement this function."
+find array = find' (bounds array) array
+    where
+        find' :: Ord a => (Int, Int) -> Array Int a -> a -> Maybe Int
+        find' (b,e) arr a
+            | e >= b && a == xm = Just m
+            | e >= b && a <  xm = find' (b, en) arr a
+            | e >= b && a >  xm = find' (bn, e) arr a
+            | otherwise         = Nothing
+            where
+                m  = (e + b) `div` 2
+                xm = arr ! m
+                bn = m + 1
+                en = m - 1
+
