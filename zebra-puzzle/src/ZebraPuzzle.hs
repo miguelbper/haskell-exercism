aaff9afd4e00c542f12e6618a9b7905eca8f9152
miguelbpgh <mbpereira94@gmail.com>
2021-04-08 23:13:28 +0100

    zebra

--- zebra-puzzle/src/ZebraPuzzle.hs
+++ zebra-puzzle/src/ZebraPuzzle.hs
@@ -1,13 +1,14 @@
 module ZebraPuzzle (Resident(..), Solution(..), solve) where
 
-import Data.List ( permutations, findIndex )
+import Control.Monad ( guard )
+import Data.List ( permutations, elemIndex )
 import Control.Applicative ( Applicative(liftA2) )
 
-data Resident  = Englishman | Spaniard | Ukrainian    | Norwegian   | Japanese    deriving (Eq, Show, Enum)
-data Color     = Red        | Green    | Ivory        | Yellow      | Blue        deriving (Eq, Show, Enum)
-data Pet       = Dog        | Snails   | Fox          | Horse       | Zebra       deriving (Eq, Show, Enum)
-data Beverage  = Coffee     | Tea      | Milk         | Juice       | Water       deriving (Eq, Show, Enum)
-data Cigarette = OldGold    | Kools    | Chesterfield | LuckyStrike | Parliaments deriving (Eq, Show, Enum)
+data Resident  = Englishman | Spaniard | Ukrainian    | Norwegian   | Japanese    deriving (Eq, Show, Bounded, Enum)
+data Color     = Red        | Green    | Ivory        | Yellow      | Blue        deriving (Eq, Show, Bounded, Enum)
+data Pet       = Dog        | Snails   | Fox          | Horse       | Zebra       deriving (Eq, Show, Bounded, Enum)
+data Beverage  = Coffee     | Tea      | Milk         | Juice       | Water       deriving (Eq, Show, Bounded, Enum)
+data Cigarette = OldGold    | Kools    | Chesterfield | LuckyStrike | Parliaments deriving (Eq, Show, Bounded, Enum)
 
 data House = House { resident  :: Resident,
                      color     :: Color,
@@ -19,55 +20,47 @@ data Solution = Solution { waterDrinker :: Resident
                          , zebraOwner :: Resident
                          } deriving (Eq, Show)
 
-type Candidate = [House]
+solve :: Solution
+solve = Solution waterResident zebraResident
+    where
+        waterResident = resident . head . filter ((==) Water . beverage) . head $ solutions
+        zebraResident = resident . head . filter ((==) Zebra . pet)      . head $ solutions 
 
-residents  = [ Englishman , Spaniard , Ukrainian    , Norwegian   , Japanese ]
-colors     = [ Red        , Green    , Ivory        , Yellow      , Blue ]
-pets       = [ Dog        , Snails   , Fox          , Horse       , Zebra ]
-beverages  = [ Coffee     , Tea      , Milk         , Juice       , Water ]
-cigarettes = [ OldGold    , Kools    , Chesterfield , LuckyStrike , Parliaments ]
-
-permResidents  = permutations [ Englishman , Spaniard , Ukrainian    , Norwegian   , Japanese ]
-permColors     = permutations [ Red        , Green    , Ivory        , Yellow      , Blue ]
-permPets       = permutations [ Dog        , Snails   , Fox          , Horse       , Zebra ]
-permBeverages  = permutations [ Coffee     , Tea      , Milk         , Juice       , Water ]
-permCigarettes = permutations [ OldGold    , Kools    , Chesterfield , LuckyStrike , Parliaments ]
+perm :: (Bounded a, Enum a) => [[a]]
+perm = permutations [ minBound .. maxBound ]
 
 myZip :: (a -> b -> c -> d -> e  -> f) -> [a] -> [b] -> [c] -> [d] -> [e] -> [f]
-myZip f [] _ _ _ _ = []
-myZip f _ [] _ _ _ = []
-myZip f _ _ [] _ _ = []
-myZip f _ _ _ [] _ = []
-myZip f _ _ _ _ [] = []
-myZip f (a:as) (b:bs) (c:cs) (d:ds) (e:es) = f a b c d e : myZip f as bs cs ds es 
+myZip _ [] _ _ _ _ = []
+myZip _ _ [] _ _ _ = []
+myZip _ _ _ [] _ _ = []
+myZip _ _ _ _ [] _ = []
+myZip _ _ _ _ _ [] = []
+myZip f (a:as) (b:bs) (c:cs) (d:ds) (e:es) = f a b c d e : myZip f as bs cs ds es
 
+solutions :: [[House]]
+solutions = do
+    resid <- (perm :: [[Resident]])
+    guard ( Norwegian == head resid) -- 10
 
-solve :: Solution
-solve = error "You need to implement this function."
+    color <- (perm :: [[Color]])
+    guard ( (Englishman, Red) `elem` zip resid color ) -- 2
+    guard ( liftA2 (-) (elemIndex Green color) (elemIndex Ivory color) == Just 1 ) -- 6
+    guard ( liftA2 (-) (elemIndex Norwegian resid) (elemIndex Blue color) `elem` [Just 1, Just (-1)] ) -- 15
 
-candidates :: [Candidate]
-candidates = [ myZip House rs cl pt bv cg | rs <- permResidents,
-                                            cl <- permColors,
-                                            pt <- permPets,
-                                            bv <- permBeverages,
-                                            cg <- permCigarettes ]
+    pet   <- (perm :: [[Pet]])
+    guard ( (Spaniard, Dog) `elem` zip resid pet) -- 3
+    
+    bevrg <- (perm :: [[Beverage]])
+    guard ( (Coffee, Green) `elem` zip bevrg color) -- 4
+    guard ( (Ukrainian, Tea) `elem` zip resid bevrg) -- 5
+    guard ( Milk == bevrg !! 2 ) -- 9
 
-isSolution :: Candidate -> Bool
-isSolution candidate = 
-       any (\h -> resident  h == Englishman  && color     h == Red )         candidate -- 2
-    && any (\h -> resident  h == Spaniard    && pet       h == Dog )         candidate -- 3
-    && any (\h -> beverage  h == Coffee      && color     h == Green )       candidate -- 4
-    && any (\h -> resident  h == Ukrainian   && beverage  h == Tea )         candidate -- 5
-    && any (\h -> cigarette h == OldGold     && pet       h == Snails )      candidate -- 7
-    && any (\h -> cigarette h == Kools       && color     h == Yellow )      candidate -- 8
-    && any (\h -> cigarette h == LuckyStrike && beverage  h == Juice )       candidate -- 13
-    && any (\h -> resident  h == Japanese    && cigarette h == Parliaments ) candidate -- 14
-    && Milk      == beverage (candidate !! 2) -- 9
-    && Norwegian == resident (head candidate) -- 10
-    && liftA2 (-) (findIndex (\h -> color h     == Green)        candidate) (findIndex (\h -> color h == Ivory) candidate) == Just 1 -- 6
-    && liftA2 (-) (findIndex (\h -> cigarette h == Chesterfield) candidate) (findIndex (\h -> pet h   == Fox)   candidate) `elem` [Just 1, Just (-1)] -- 11
-    && liftA2 (-) (findIndex (\h -> cigarette h == Kools)        candidate) (findIndex (\h -> pet h   == Horse) candidate) `elem` [Just 1, Just (-1)] -- 12
-    && liftA2 (-) (findIndex (\h -> resident h  == Norwegian)    candidate) (findIndex (\h -> color h == Blue)  candidate) `elem` [Just 1, Just (-1)] -- 15
+    cigar <- (perm :: [[Cigarette]])
+    guard ( (OldGold, Snails) `elem` zip cigar pet) -- 7
+    guard ( (Kools, Yellow) `elem` zip cigar color) -- 8
+    guard ( (LuckyStrike, Juice) `elem` zip cigar bevrg) -- 13
+    guard ( (Japanese, Parliaments) `elem` zip resid cigar) -- 14
+    guard ( liftA2 (-) (elemIndex Chesterfield cigar) (elemIndex Fox pet) `elem` [Just 1, Just (-1)] ) -- 11
+    guard ( liftA2 (-) (elemIndex Kools cigar) (elemIndex Horse pet) `elem` [Just 1, Just (-1)] ) -- 12
 
-solutions :: [Candidate]
-solutions = filter isSolution candidates
+    return (myZip House resid color pet bevrg cigar)
