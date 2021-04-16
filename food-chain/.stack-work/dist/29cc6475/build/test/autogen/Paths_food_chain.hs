a87cb387286c056a9f47cad03b20bf012039351b
miguelbpgh <mbpereira94@gmail.com>
2021-04-16 20:03:45 +0100

    food chain

---
+++ food-chain/.stack-work/dist/29cc6475/build/test/autogen/Paths_food_chain.hs
@@ -0,0 +1,50 @@
+{-# LANGUAGE CPP #-}
+{-# LANGUAGE NoRebindableSyntax #-}
+{-# OPTIONS_GHC -fno-warn-missing-import-lists #-}
+module Paths_food_chain (
+    version,
+    getBinDir, getLibDir, getDynLibDir, getDataDir, getLibexecDir,
+    getDataFileName, getSysconfDir
+  ) where
+
+import qualified Control.Exception as Exception
+import Data.Version (Version(..))
+import System.Environment (getEnv)
+import Prelude
+
+#if defined(VERSION_base)
+
+#if MIN_VERSION_base(4,0,0)
+catchIO :: IO a -> (Exception.IOException -> IO a) -> IO a
+#else
+catchIO :: IO a -> (Exception.Exception -> IO a) -> IO a
+#endif
+
+#else
+catchIO :: IO a -> (Exception.IOException -> IO a) -> IO a
+#endif
+catchIO = Exception.catch
+
+version :: Version
+version = Version [0,1,0,3] []
+bindir, libdir, dynlibdir, datadir, libexecdir, sysconfdir :: FilePath
+
+bindir     = "C:\\Users\\Miguel\\Exercism\\haskell\\food-chain\\.stack-work\\install\\e0f1c74b\\bin"
+libdir     = "C:\\Users\\Miguel\\Exercism\\haskell\\food-chain\\.stack-work\\install\\e0f1c74b\\lib\\x86_64-windows-ghc-8.8.4\\food-chain-0.1.0.3-1kDXOyHVi8R62QZocZPBds-test"
+dynlibdir  = "C:\\Users\\Miguel\\Exercism\\haskell\\food-chain\\.stack-work\\install\\e0f1c74b\\lib\\x86_64-windows-ghc-8.8.4"
+datadir    = "C:\\Users\\Miguel\\Exercism\\haskell\\food-chain\\.stack-work\\install\\e0f1c74b\\share\\x86_64-windows-ghc-8.8.4\\food-chain-0.1.0.3"
+libexecdir = "C:\\Users\\Miguel\\Exercism\\haskell\\food-chain\\.stack-work\\install\\e0f1c74b\\libexec\\x86_64-windows-ghc-8.8.4\\food-chain-0.1.0.3"
+sysconfdir = "C:\\Users\\Miguel\\Exercism\\haskell\\food-chain\\.stack-work\\install\\e0f1c74b\\etc"
+
+getBinDir, getLibDir, getDynLibDir, getDataDir, getLibexecDir, getSysconfDir :: IO FilePath
+getBinDir = catchIO (getEnv "food_chain_bindir") (\_ -> return bindir)
+getLibDir = catchIO (getEnv "food_chain_libdir") (\_ -> return libdir)
+getDynLibDir = catchIO (getEnv "food_chain_dynlibdir") (\_ -> return dynlibdir)
+getDataDir = catchIO (getEnv "food_chain_datadir") (\_ -> return datadir)
+getLibexecDir = catchIO (getEnv "food_chain_libexecdir") (\_ -> return libexecdir)
+getSysconfDir = catchIO (getEnv "food_chain_sysconfdir") (\_ -> return sysconfdir)
+
+getDataFileName :: FilePath -> IO FilePath
+getDataFileName name = do
+  dir <- getDataDir
+  return (dir ++ "\\" ++ name)
--- food-chain/src/FoodChain.hs
+++ food-chain/src/FoodChain.hs
@@ -1,54 +1,47 @@
 module FoodChain (song) where
 
+type Animal = String
+type Lyric  = String
+
+animals :: [Animal]
+animals = ["cow","goat","dog","cat","bird","spider","fly"]
+
+firstLyric :: Animal -> Lyric
+firstLyric a = "I know an old lady who swallowed a " ++ a ++ ".\n"
+
+specialLyric :: Animal -> Lyric
+specialLyric a = case a of
+    "cow"    -> "I don't know how she swallowed a cow!\n"
+    "goat"   -> "Just opened her throat and swallowed a goat!\n"
+    "dog"    -> "What a hog, to swallow a dog!\n"
+    "cat"    -> "Imagine that, to swallow a cat!\n"
+    "bird"   -> "How absurd to swallow a bird!\n"
+    "spider" -> "It wriggled and jiggled and tickled inside her.\n"
+    _        -> ""
+
+spiderLyric :: Animal -> Lyric
+spiderLyric "spider" = "spider that wriggled and jiggled and tickled inside her"
+spiderLyric a        = a
+
+swallowedLyric :: Animal -> Animal -> Lyric
+swallowedLyric a b = "She swallowed the " ++ a ++ " to catch the " ++ spiderLyric b ++ ".\n"
+
+endLyric :: [Animal] -> Lyric
+endLyric []       = ""
+endLyric ["fly"]  = "I don't know why she swallowed the fly. Perhaps she'll die.\n\n"
+endLyric [_]      = ""
+endLyric (a:b:as) = swallowedLyric a b ++ endLyric (b:as)
+
+songBlock :: [Animal] -> Lyric
+songBlock []     = ""
+songBlock (a:as) = firstLyric a ++ specialLyric a ++ endLyric (a:as)
+
+lastLyric :: Lyric
+lastLyric = "I know an old lady who swallowed a horse.\nShe's dead, of course!\n"
+
 song :: String
-song =
-    "I know an old lady who swallowed a fly.\n\
-    \I don't know why she swallowed the fly. Perhaps she'll die.\n\
-    \\n\
-    \I know an old lady who swallowed a spider.\n\
-    \It wriggled and jiggled and tickled inside her.\n\
-    \She swallowed the spider to catch the fly.\n\
-    \I don't know why she swallowed the fly. Perhaps she'll die.\n\
-    \\n\
-    \I know an old lady who swallowed a bird.\n\
-    \How absurd to swallow a bird!\n\
-    \She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.\n\
-    \She swallowed the spider to catch the fly.\n\
-    \I don't know why she swallowed the fly. Perhaps she'll die.\n\
-    \\n\
-    \I know an old lady who swallowed a cat.\n\
-    \Imagine that, to swallow a cat!\n\
-    \She swallowed the cat to catch the bird.\n\
-    \She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.\n\
-    \She swallowed the spider to catch the fly.\n\
-    \I don't know why she swallowed the fly. Perhaps she'll die.\n\
-    \\n\
-    \I know an old lady who swallowed a dog.\n\
-    \What a hog, to swallow a dog!\n\
-    \She swallowed the dog to catch the cat.\n\
-    \She swallowed the cat to catch the bird.\n\
-    \She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.\n\
-    \She swallowed the spider to catch the fly.\n\
-    \I don't know why she swallowed the fly. Perhaps she'll die.\n\
-    \\n\
-    \I know an old lady who swallowed a goat.\n\
-    \Just opened her throat and swallowed a goat!\n\
-    \She swallowed the goat to catch the dog.\n\
-    \She swallowed the dog to catch the cat.\n\
-    \She swallowed the cat to catch the bird.\n\
-    \She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.\n\
-    \She swallowed the spider to catch the fly.\n\
-    \I don't know why she swallowed the fly. Perhaps she'll die.\n\
-    \\n\
-    \I know an old lady who swallowed a cow.\n\
-    \I don't know how she swallowed a cow!\n\
-    \She swallowed the cow to catch the goat.\n\
-    \She swallowed the goat to catch the dog.\n\
-    \She swallowed the dog to catch the cat.\n\
-    \She swallowed the cat to catch the bird.\n\
-    \She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.\n\
-    \She swallowed the spider to catch the fly.\n\
-    \I don't know why she swallowed the fly. Perhaps she'll die.\n\
-    \\n\
-    \I know an old lady who swallowed a horse.\n\
-    \She's dead, of course!\n"
+song = concatMap songBlock ( listOfLast  animals ) ++ lastLyric
+
+listOfLast :: [a] -> [[a]]
+listOfLast []     = []
+listOfLast (x:xs) = listOfLast xs ++ [x:xs]
