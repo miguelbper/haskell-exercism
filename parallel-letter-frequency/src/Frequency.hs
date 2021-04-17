763e44ee992c6ef77872d73b9b7da5739505938e
miguelbpgh <mbpereira94@gmail.com>
2021-04-17 22:27:16 +0100

    parallel letter frequency

--- parallel-letter-frequency/src/Frequency.hs
+++ parallel-letter-frequency/src/Frequency.hs
@@ -1,7 +1,21 @@
 module Frequency (frequency) where
 
-import Data.Map  (Map)
-import Data.Text (Text)
+import Data.Char ( isAlpha )
+import Data.Map ( Map, empty, insertWith, unionsWith )
+import Data.Text as T ( Text, foldr, toLower, filter )
+import Control.Parallel.Strategies ( using, rdeepseq, parListChunk )
+
+format :: Text -> Text
+format = T.filter isAlpha . toLower
+
+addToMap :: (Ord k) => k -> Map k Int -> Map k Int
+addToMap = flip (insertWith (+)) 1
+ 
+frequency' :: Text -> Map Char Int
+frequency' = T.foldr addToMap empty . format
 
 frequency :: Int -> [Text] -> Map Char Int
-frequency nWorkers texts = error "You need to implement this function."
+frequency nWorkers texts = unionsWith (+) ( using (map frequency' texts) strat )
+    where
+        chunkSize = length texts `div` nWorkers
+        strat = parListChunk chunkSize rdeepseq
