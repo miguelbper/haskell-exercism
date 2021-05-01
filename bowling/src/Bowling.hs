6a034ba5fee605fdf51887ce3ca305bf6066111d
miguelbpgh <mbpereira94@gmail.com>
2021-05-01 17:29:32 +0100

    bowling mid tests change str

--- bowling/src/Bowling.hs
+++ bowling/src/Bowling.hs
@@ -1,6 +1,6 @@
 module Bowling (score, BowlingError(..)) where
 
-import Control.Monad ( liftM2 )
+import Control.Monad ( liftM2, (<=<) )
 
 data BowlingError = IncompleteGame
                   | InvalidRoll { rollIndex :: Int, rollValue :: Int }
@@ -18,40 +18,37 @@ data FrameSc  = Error | First | Fill | Open | Spare | Strike deriving (Eq, Show)
 
 -- main functions
 score :: Throws -> Either BowlingError Score
-score ts = if cl then sc else Left IncompleteGame
+score = score' <=< (findErrors . gameFromThrows)
+
+findErrors :: Game -> Either BowlingError Game
+findErrors gs = if null errors then Right gs else Left (InvalidRoll eii etv)
     where
-        gs = gameFromThrows ts
-        cl = complete gs
-        sc = score' gs
+        errors = filter (\(_, _, _, yfs) -> yfs == Error) gs
+        (_, eii, etv, _) = head errors
 
 score' :: Game -> Either BowlingError Score
 score' []     = Right 0
-score' (x:xs) = if condition then result else invRoll
+score' (x:xs) = if condition then result else (Left IncompleteGame)
     where
         y = xs !! 0
         z = xs !! 1
 
-        (_  , xii, xtv, xfs) = x
+        (_  , _  , xtv, xfs) = x
         (_  , _  , ytv, _  ) = y
         (_  , _  , ztv, _  ) = z
 
         condition
-            | xfs == Error  = False
             | xfs == Strike = length xs >= 2
             | xfs == Spare  = length xs >= 1
             | otherwise     = True
 
         scr
+            | xfs == Fill   = 0
             | xfs == Strike = xtv + ytv + ztv
             | xfs == Spare  = xtv + ytv
             | otherwise     = xtv
 
-        invRoll = Left (InvalidRoll xii xtv)
-
         result = liftM2 (+) (Right scr) (score' xs)
-
-complete :: Game -> Bool
-complete = const True
 
 -- convert
 gameFromThrows :: Throws -> Game
@@ -60,11 +57,11 @@ gameFromThrows = foldl addThrow []
 addThrow :: Game -> ThrowVal -> Game
 addThrow gs x = gs ++ [xx]
     where
-        (yfn, yii, ytv, yfs) = if null gs then (0,0,0,Open) else last gs
+        (yfn, yii, ytv, yfs) = if null gs then (0,-1,0,Open) else last gs
 
         toNewFrame = yfs /= First && yfn /= 10
-        isFill     = (yfs == Strike || yfs == Spare) && yfn == 10
-        frameScore = xtv + ytv * fromEnum (not toNewFrame)
+        isFill     = (yfs == Strike || yfs == Spare || yfs == Fill) && yfn == 10
+        frameScore = xtv + ytv * fromEnum (not (toNewFrame || isFill))
 
         xtv = x
         xii = yii + 1
