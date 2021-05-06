c2d2da3b8e575e914567387031201205f4f46a13
miguelbpgh <mbpereira94@gmail.com>
2021-05-06 14:19:20 +0100

    go done! starting with lens

---
+++ go-counting/.stack-work/dist/29cc6475/build/autogen/Paths_go_counting.hs
@@ -0,0 +1,50 @@
+{-# LANGUAGE CPP #-}
+{-# LANGUAGE NoRebindableSyntax #-}
+{-# OPTIONS_GHC -fno-warn-missing-import-lists #-}
+module Paths_go_counting (
+    version,
+    getBinDir, getLibDir, getDynLibDir, getDataDir, getLibexecDir,
+    getDataFileName, getSysconfDir
+  ) where
+
+import qualified Control.Exception as Exception
+import Data.Version (Version(..))
+import System.Environment (getEnv)
+import Prelude
+
+#if defined(VERSION_base)
+
+#if MIN_VERSION_base(4,0,0)
+catchIO :: IO a -> (Exception.IOException -> IO a) -> IO a
+#else
+catchIO :: IO a -> (Exception.Exception -> IO a) -> IO a
+#endif
+
+#else
+catchIO :: IO a -> (Exception.IOException -> IO a) -> IO a
+#endif
+catchIO = Exception.catch
+
+version :: Version
+version = Version [1,0,0,4] []
+bindir, libdir, dynlibdir, datadir, libexecdir, sysconfdir :: FilePath
+
+bindir     = "C:\\Users\\Miguel\\Exercism\\haskell\\go-counting\\.stack-work\\install\\e0f1c74b\\bin"
+libdir     = "C:\\Users\\Miguel\\Exercism\\haskell\\go-counting\\.stack-work\\install\\e0f1c74b\\lib\\x86_64-windows-ghc-8.8.4\\go-counting-1.0.0.4-5l4LIX9qTQIIL7IzrGJSs7"
+dynlibdir  = "C:\\Users\\Miguel\\Exercism\\haskell\\go-counting\\.stack-work\\install\\e0f1c74b\\lib\\x86_64-windows-ghc-8.8.4"
+datadir    = "C:\\Users\\Miguel\\Exercism\\haskell\\go-counting\\.stack-work\\install\\e0f1c74b\\share\\x86_64-windows-ghc-8.8.4\\go-counting-1.0.0.4"
+libexecdir = "C:\\Users\\Miguel\\Exercism\\haskell\\go-counting\\.stack-work\\install\\e0f1c74b\\libexec\\x86_64-windows-ghc-8.8.4\\go-counting-1.0.0.4"
+sysconfdir = "C:\\Users\\Miguel\\Exercism\\haskell\\go-counting\\.stack-work\\install\\e0f1c74b\\etc"
+
+getBinDir, getLibDir, getDynLibDir, getDataDir, getLibexecDir, getSysconfDir :: IO FilePath
+getBinDir = catchIO (getEnv "go_counting_bindir") (\_ -> return bindir)
+getLibDir = catchIO (getEnv "go_counting_libdir") (\_ -> return libdir)
+getDynLibDir = catchIO (getEnv "go_counting_dynlibdir") (\_ -> return dynlibdir)
+getDataDir = catchIO (getEnv "go_counting_datadir") (\_ -> return datadir)
+getLibexecDir = catchIO (getEnv "go_counting_libexecdir") (\_ -> return libexecdir)
+getSysconfDir = catchIO (getEnv "go_counting_sysconfdir") (\_ -> return sysconfdir)
+
+getDataFileName :: FilePath -> IO FilePath
+getDataFileName name = do
+  dir <- getDataDir
+  return (dir ++ "\\" ++ name)
--- go-counting/src/Counting.hs
+++ go-counting/src/Counting.hs
@@ -4,13 +4,80 @@ module Counting (
     territoryFor
 ) where
 
-import Data.Set (Set)
+import Data.Set (Set, empty, union, singleton)
+import Data.List (nub)
+import Data.Maybe (catMaybes)
 
-data Color = Black | White deriving (Eq, Ord, Show)
-type Coord = (Int, Int)
+data Color     = Empty | Black | White deriving (Eq, Ord, Show)
+type Coord     = (Int, Int)
+type Board     = [String]
+type Territory = (Set Coord, Maybe Color)
 
-territories :: [String] -> [(Set Coord, Maybe Color)]
-territories board = error "You need to implement this function."
+-- main functions
+territories :: Board -> [Territory]
+territories board = nub . catMaybes $ [ territoryFor board (a,b) | a <- [1..x], b <- [1..y] ]
+    where
+        x = if null board then 0 else length (head board)
+        y = length board
 
-territoryFor :: [String] -> Coord -> Maybe (Set Coord, Maybe Color)
-territoryFor board coord = error "You need to implement this function."
+territoryFor :: Board -> Coord -> Maybe Territory
+territoryFor board coord
+    | coord `notElem` brd       = Nothing
+    | clr `elem` [Black, White] = Nothing
+    | otherwise                 = Just (nothingFromEmpty <$> territory board [] coord)  
+    where
+        x   = if null board then 0 else length (head board)
+        y   = length board
+        brd = [ (m,n) | m <- [1..x], n <- [1..y] ]
+        clr = colorOfcoord board coord
+
+territory :: Board -> [Coord] -> Coord -> Territory
+territory board cs c
+    | clr == White = (empty, Just White)
+    | clr == Black = (empty, Just Black)
+    | otherwise    = concatT (crdT : nbdT)    
+    where
+        clr = colorOfcoord board c
+        nbd = filter (`notElem` cs) $ neighbours board c
+
+        crdT = (singleton c, Just Empty)
+        nbdT = map (territory board (c:cs)) nbd
+
+-- sum territory
+sumC :: Maybe Color -> Maybe Color -> Maybe Color
+sumC x y
+    | x == Just White = if y == Just White || y == Just Empty then Just White else Nothing
+    | x == Just Black = if y == Just Black || y == Just Empty then Just Black else Nothing
+    | x == Just Empty = y
+    | otherwise       = Nothing
+
+sumT :: Territory -> Territory -> Territory
+sumT (a,b) (c,d) = (a `union` c, b `sumC` d)
+
+concatT :: [Territory] -> Territory
+concatT = foldr sumT (empty, Just Empty)
+
+-- auxiliary functions
+neighbours :: Board -> Coord -> [Coord]
+neighbours board (a,b) = filter (`elem` brd) nbs
+    where
+        x   = if null board then 0 else length (head board)
+        y   = length board
+        brd = [ (m,n) | m <- [1..x], n <- [1..y] ]
+        nbs = [ (a - 1, b    ),
+                (a + 1, b    ),
+                (a    , b - 1),
+                (a    , b + 1) ]
+
+toColor :: Char -> Color
+toColor c = case c of
+    'W' -> White
+    'B' -> Black
+    _   -> Empty
+
+colorOfcoord :: Board -> Coord -> Color
+colorOfcoord board (a,b) = toColor $ (board !! (b - 1)) !! (a - 1)
+
+nothingFromEmpty :: Maybe Color -> Maybe Color
+nothingFromEmpty (Just Empty) = Nothing
+nothingFromEmpty x            = x
