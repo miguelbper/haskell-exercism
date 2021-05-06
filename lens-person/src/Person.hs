2a83148f3c5fd6862f66926b5317173d5aa7f6de
miguelbpgh <mbpereira94@gmail.com>
2021-05-06 16:12:05 +0100

    person

--- lens-person/src/Person.hs
+++ lens-person/src/Person.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE TemplateHaskell #-}
 module Person
   ( Address (..)
   , Born    (..)
@@ -9,7 +10,9 @@ module Person
   , setCurrentStreet
   ) where
 
-import Data.Time.Calendar (Day)
+import Data.Time.Calendar (Day, fromGregorian, toGregorian)
+import Control.Monad ( liftM2 )
+import Control.Lens (makeLenses, set, over)
 
 data Person = Person { _name    :: Name
                      , _born    :: Born
@@ -30,14 +33,23 @@ data Address = Address { _street      :: String
                        , _country     :: String
                        }
 
+type Month = Int
+
+makeLenses ''Person
+makeLenses ''Born
+makeLenses ''Address
+
 bornStreet :: Born -> String
-bornStreet born = error "You need to implement this function."
+bornStreet = _street . _bornAt
 
 setCurrentStreet :: String -> Person -> Person
-setCurrentStreet street person = error "You need to implement this function."
+setCurrentStreet = set (address . street)
 
-setBirthMonth :: Int -> Person -> Person
-setBirthMonth month person = error "You need to implement this function."
+setBirthMonth :: Month -> Person -> Person
+setBirthMonth = over born . over bornOn . monthChanger
 
 renameStreets :: (String -> String) -> Person -> Person
-renameStreets f person = error "You need to implement this function."
+renameStreets = liftM2 (.) (over address . over street) (over born . over bornAt . over street)
+
+monthChanger :: Month -> Day -> Day
+monthChanger m dy = let (y, _, d) = toGregorian dy in fromGregorian y m d
