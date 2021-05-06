3f4a08954cad62cd845b3cd44dba99d0afe513e9
miguelbpgh <mbpereira94@gmail.com>
2021-05-06 23:44:34 +0100

    pov 1 function missing!

--- pov/src/POV.hs
+++ pov/src/POV.hs
@@ -1,9 +1,24 @@
 module POV (fromPOV, tracePathBetween) where
 
-import Data.Tree (Tree)
+import Data.Tree (Tree(Node, rootLabel), Forest)
+import Data.Maybe ( fromJust, listToMaybe )
+import Control.Monad ( join )
+import Data.List ( inits, tails )
 
 fromPOV :: Eq a => a -> Tree a -> Maybe (Tree a)
-fromPOV x tree = error "You need to implement this function."
+fromPOV x tree = inverse =<< findPath x tree
 
 tracePathBetween :: Eq a => a -> a -> Tree a -> Maybe [a]
-tracePathBetween from to tree = error "You need to implement this function."
+tracePathBetween from to tree = p2l path
+    where
+        tree' = fromPOV to tree
+        path  = findPath from =<< tree'
+        p2l   = fmap (map rootLabel)
+
+inverse :: Forest a -> Maybe (Tree a)
+inverse            []  = Nothing
+inverse (Node a ls:[]) = Just (Node a ls)
+inverse (Node a ls:ts) = Just (Node a ((fromJust . inverse $ ts) : ls))
+
+findPath :: Eq a => a -> Tree a -> Maybe (Forest a)
+findPath goal tree = error "lol"
