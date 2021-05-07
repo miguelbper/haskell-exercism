7be0e36214c690414a37e57224b7dde7ddaeb2c8
miguelbpgh <mbpereira94@gmail.com>
2021-05-07 16:23:50 +0100

    woriking sol

--- forth/src/Forth.hs
+++ forth/src/Forth.hs
@@ -7,13 +7,15 @@ module Forth
   , emptyState
   ) where
 
-import qualified Data.Char as C (isDigit)
+import Data.Char (isDigit, toUpper)
+import Data.Bool
 import Data.Text (Text, toLower, pack)
-import qualified Data.Text as T (head)
+import qualified Data.Text as T (head, words, map, all, null)
+import Data.Text.Read as TR (signed, decimal)
 import Data.Map (Map, fromList, insert)
-import qualified Data.Map as M (lookup)
+import qualified Data.Map as M (lookup, insert)
 import Control.Applicative (Alternative((<|>)))
-import Control.Monad (foldM)
+import Control.Monad (foldM, (>=>))
 import Data.Attoparsec.Text (sepBy, anyChar, Parser, parseOnly, takeWhile1, manyTill, skipSpace, choice, string, decimal, char, (<?>))
 
 data ForthError
@@ -43,10 +45,10 @@ defaultActions = fromList [ ("+"   , binaryOp (+))
                           , ("-"   , binaryOp (-))
                           , ("*"   , binaryOp (*))
                           , ("/"   , division    )
-                          , ("dup" , oneElemList (\x   -> [x,x]  ))
-                          , ("drop", oneElemList (\x   -> []     ))
-                          , ("swap", twoElemList (\x y -> [x,y]  ))
-                          , ("over", twoElemList (\x y -> [x,y,x])) ]
+                          , ("DUP" , oneElemList (\x   -> [x,x]  ))
+                          , ("DROP", oneElemList (\x   -> []     ))
+                          , ("SWAP", twoElemList (\x y -> [x,y]  ))
+                          , ("OVER", twoElemList (\x y -> [x,y,x])) ]
 
 binaryOp :: (Int -> Int -> Int) -> Action
 binaryOp op (x:y:xs) = Right (op y x : xs)
@@ -67,4 +69,45 @@ twoElemList _  _        = Left StackUnderflow
 
 -- eval
 evalText :: Text -> ForthState -> Either ForthError ForthState
-evalText text state = error "l"
+evalText text state = eval state (T.words text)
+
+eval :: ForthState -> [Text] -> Either ForthError ForthState
+eval st [] = Right st
+eval st (w:ws) = case T.map toUpper w of
+  ":" -> case break (";"==) ws of
+           (ws',_:ws'') -> either Left (flip eval ws'') (updateDictWith ws' st)
+           _            -> Left InvalidWord
+  wd | T.all isDigit wd -> eval (st { stack = decimalToIntegral wd : stack st }) ws
+     | otherwise        -> case M.lookup wd (actions st) of
+                             Nothing -> Left $ UnknownWord w
+                             Just f  -> either Left (flip eval ws) (updateStack f st)
+
+updateStack :: ([Int] -> Either ForthError [Int]) -> ForthState -> Either ForthError ForthState
+updateStack s st = either Left (Right . f) (s $ stack st)
+  where
+    f stk = st { stack = stk }
+
+updateDictWith :: [Text] -> ForthState -> Either ForthError ForthState
+updateDictWith wws st = case map (T.map toUpper) wws of
+    w:ws | T.all isDigit w -> Left InvalidWord
+         | otherwise       -> case mkop (actions st) ws of
+             Left e  -> Left e
+             Right f -> Right $ st { actions = M.insert (T.map toUpper w) f (actions st) }
+
+mkop :: Map Text Action -> [Text] -> Either ForthError Action
+mkop dic = foldr f (Right pure)
+  where
+    f x eop = if T.all isDigit x
+              then composeOP (pure . (decimalToIntegral x :)) eop
+              else case M.lookup x dic of
+                     Nothing -> Left (UnknownWord x)
+                     Just op -> composeOP op eop
+
+decimalToIntegral :: Integral a => Text -> a
+decimalToIntegral t = either (error . id)
+                             (\ (i,rt) -> bool (error "invalid input") i (T.null rt))
+                             (TR.signed TR.decimal t)
+
+composeOP :: Action -> Either ForthError Action -> Either ForthError Action
+composeOP op (Right op') = Right (op >=> op')
+composeOp _  eop         = eop
