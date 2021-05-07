a5b3d6e7632025c48c7b78644399226e7d909347
miguelbpgh <mbpereira94@gmail.com>
2021-05-07 12:28:34 +0100

    forth empty

--- forth/src/Forth.hs
+++ forth/src/Forth.hs
@@ -9,6 +9,7 @@ module Forth
   ) where
 
 import Data.Text (Text)
+import Data.Map (Map, fromList)
 
 data ForthError
      = DivisionByZero
@@ -17,13 +18,49 @@ data ForthError
      | UnknownWord Text
      deriving (Show, Eq)
 
-data ForthState = Dummy
+-- types
+type Stack      = [Int]
+type Operation  = Text
+type Action     = ForthState -> Either ForthError ForthState
+data ForthState = ForthState { stack   :: Stack, 
+                               actions :: Map Operation Action}
 
+-- list
+toList :: ForthState -> [Int]
+toList = reverse . stack
+
+-- empty
 emptyState :: ForthState
-emptyState = error "You need to implement this function."
+emptyState = ForthState [] defaultActions
 
+defaultActions :: Map Operation Action
+defaultActions = fromList [ ("+"   , binaryOp (+))
+                          , ("-"   , binaryOp (-))
+                          , ("*"   , binaryOp (*))
+                          , ("/"   , division    )
+                          , ("dup" , oneElemList (\x   -> [x,x]  ))
+                          , ("drop", oneElemList (\x   -> []     ))
+                          , ("swap", twoElemList (\x y -> [y,x]  ))
+                          , ("over", twoElemList (\x y -> [x,y,x])) ]
+
+binaryOp :: (Int -> Int -> Int) -> Action
+binaryOp op (ForthState (x:y:xs) act) = Right (ForthState (op y x : xs) act)
+binaryOp _  _                         = Left StackUnderflow
+
+division :: Action
+division (ForthState (0:y:xs) act) = Left DivisionByZero
+division (ForthState (x:y:xs) act) = Right (ForthState (div y x : xs) act)
+division _                         = Left StackUnderflow
+
+oneElemList :: (Int -> Stack) -> Action
+oneElemList op (ForthState (x:xs) act) = Right (ForthState (op x ++ xs) act)
+oneElemList _  _                       = Left StackUnderflow
+
+twoElemList :: (Int -> Int -> Stack) -> Action
+twoElemList op (ForthState (x:y:xs) act) = Right (ForthState (op x y ++ xs) act)
+twoElemList _  _                         = Left StackUnderflow
+
+-- eval
 evalText :: Text -> ForthState -> Either ForthError ForthState
 evalText text stack = error "You need to implement this function."
 
-toList :: ForthState -> [Int]
-toList stack = error "You need to implement this function."
